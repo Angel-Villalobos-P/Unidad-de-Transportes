b84b4f8a6951d58db1962a7904b80e4c13f62ad2
Ángel Villalobos <41980561+Angel-Villalobos-P@users.noreply.github.com>
2018-09-28 23:12:22 -0600

    Getter and setters

--- Unidad de Transportes/src/clases/Persona.java
+++ Unidad de Transportes/src/clases/Persona.java
@@ -11,5 +11,46 @@ public class Persona {
     //private Direccion direccion;
     private String correo;
     private String telefono;
+
+    public Persona(String nombre, String cedula, String correo, String telefono) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
     
 }
