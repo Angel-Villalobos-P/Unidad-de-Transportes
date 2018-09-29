1fb1f795c15dd47c6959c24baff2ac4c20507b0c
Ángel Villalobos <41980561+Angel-Villalobos-P@users.noreply.github.com>
2018-09-28 23:12:35 -0600

    Getter and setters

--- Unidad de Transportes/src/clases/Licencia.java
+++ Unidad de Transportes/src/clases/Licencia.java
@@ -10,5 +10,46 @@ public class Licencia {
     private String fechaEmision;
     private String tipo;
     private String fechaExpiracion;
+
+    public Licencia(int numero, String fechaEmision, String tipo, String fechaExpiracion) {
+        this.numero = numero;
+        this.fechaEmision = fechaEmision;
+        this.tipo = tipo;
+        this.fechaExpiracion = fechaExpiracion;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public void setFechaEmision(String fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getFechaExpiracion() {
+        return fechaExpiracion;
+    }
+
+    public void setFechaExpiracion(String fechaExpiracion) {
+        this.fechaExpiracion = fechaExpiracion;
+    }
+    
+    
     
 }
