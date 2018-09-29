af87806b648abe690d007087aa273292a66cd585
Ángel Villalobos <41980561+Angel-Villalobos-P@users.noreply.github.com>
2018-09-28 23:12:04 -0600

    Getter and setters

--- Unidad de Transportes/src/clases/Empresa.java
+++ Unidad de Transportes/src/clases/Empresa.java
@@ -10,5 +10,46 @@ public class Empresa {
     private String cedulaJuridica;
     private String telefono;
     private String direccion;
+
+    public Empresa(String razonSocial, String cedulaJuridica, String telefono, String direccion) {
+        this.razonSocial = razonSocial;
+        this.cedulaJuridica = cedulaJuridica;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getCedulaJuridica() {
+        return cedulaJuridica;
+    }
+
+    public void setCedulaJuridica(String cedulaJuridica) {
+        this.cedulaJuridica = cedulaJuridica;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
     
 }
