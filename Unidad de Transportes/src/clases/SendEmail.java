77cd9090e3e9baa7b187eb3cdcaf87ddee573e23
FerLanza <ferlanza14@gmail.com>
2018-09-30 19:22:27 +0400

    EnviarCorreo

---
+++ Unidad de Transportes/src/clases/SendEmail.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class SendEmail {
+    private static String Username="xxxx@gmail.com";
+    private static String Password="xxxxx";
+    String Mensage="HOLA";
+    String To="ferlanza14@gmail.com";
+    String Subject="Prueba";
+  
+
+ public void SendMail() {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(Username, Password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(Username));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(To));
+            message.setSubject(Subject);
+            message.setText(Mensage);
+
+            Transport.send(message);
+            
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+   
+}
+ 
+
