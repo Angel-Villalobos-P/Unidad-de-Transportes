5af50ddcca029d9d91e13b2faa8664f56ca14238
FerAriasP <43561647+FerAriasP@users.noreply.github.com>
2018-09-30 23:28:23 -0600

    Clase Persona

--- Unidad de Transportes/src/clases/Persona.java
+++ Unidad de Transportes/src/clases/Persona.java
@@ -8,11 +8,11 @@ package clases;
 public class Persona {
     private String nombre;
     private String cedula;
-    //private Direccion direccion;
+    private Direccion direccion;
     private String correo;
     private String telefono;
 
-    public Persona(String nombre, String cedula, String correo, String telefono) {
+    public Persona(String nombre, String cedula, Direccion direccion, String correo, String telefono) {
         this.nombre = nombre;
         this.cedula = cedula;
         this.correo = correo;
