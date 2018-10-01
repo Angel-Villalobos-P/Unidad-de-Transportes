ff53e74f0cb128b202c3984fe22df5d50621f8be
FerAriasP <43561647+FerAriasP@users.noreply.github.com>
2018-09-30 23:22:00 -0600

    Clase FuncionesSecretaria

--- Unidad de Transportes/src/funciones/FuncionesSecretaria.java
+++ Unidad de Transportes/src/funciones/FuncionesSecretaria.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package funciones;
+import clases.Direccion;
+import clases.Persona;
+
 
 /**
  *
@@ -11,8 +14,18 @@ package funciones;
  */
 public class FuncionesSecretaria {
     
-    public void registrarPasajero(){
-        
+    public void registrarPasajero(String pNombre, String pCedula, Direccion pDireccion, String pCorreo, String pTelefono){
+        for(int elemento=0; elemento <= pasajeros.size()-1; elemento++){
+            Persona pasajero= pasajeros.get(elemento);
+            if(pCedula==pasajero.getCedula() ){
+                return ("Error, esiste una coincidencia de cédula con un pasajero previamente registrado");
+
+            }
+        }
+        Persona pasajero= new Persona(pNombre, pCedula, pDireccion, pCorreo, pTelefono);
+        pasajeros.add(pasajero);
+        return "El pasajero se ha registrado con éxito";
+
     }
     
     public void solicitarServicioDeViaje(){
